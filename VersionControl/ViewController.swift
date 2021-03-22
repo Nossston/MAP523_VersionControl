7c5072ea9617d7db6f83e85f0090c03809d0113d
Noston Liu <noston123123@gmail.com>
2021-03-22 09:26:14 -0400

    test

--- VersionControl/ViewController.swift
+++ VersionControl/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Hello from  viewDidLoad().")
+        print("Hello from  viewDidLoad()....")
     }
     
     
